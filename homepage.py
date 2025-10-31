330e9e15962a1896423fb248cb3177dec7fbb38f
lanhat <91308579+lanhat@users.noreply.github.com>
2025-10-31 14:58:00 +0000

    Your commit message

--- homepage.py
+++ homepage.py
@@ -14,9 +14,9 @@ show_pages([
 
     # Study cases
     Section("Study Cases", ":label:"),
-    #Page("kameda.py", "Kameda, Japan", icon=":flag-jp:", in_section=True),
+    Page("kameda.py", "Kameda, Japan", icon=":flag-jp:", in_section=True),
     Page("xuanthuy.py", "Xuan Thuy, Viet Nam", icon=":flag-vn:", in_section=True),
-    #Page("subang.py", "Subang, Indonesia", icon=":flag-id:", in_section=True),
+    Page("subang.py", "Subang, Indonesia", icon=":flag-id:", in_section=True),
 
     #Page("faq.py", "FAQ", "❔", in_section=False),
     #Page("contact.py", "Contact", icon="📩", in_section=False),
